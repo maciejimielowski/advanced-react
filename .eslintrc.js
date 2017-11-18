cbfe108d78ced4e8638351f165a23dca66b8cef6
maciejimielowski <maciej.imielowski@gmail.com>
2017-11-18 23:59:16 +0100

    Completed 1st part

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "parser": 'babel-eslint',
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true,
+    "jest": true,
+  },
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "plugins": [ "react" ],
+  "rules": {
+    "react/prop-types": ["off"],
+    "quotes": ["error","single"],
+    "semi": ["error","always"],
+    "no-console": ["warn", { "allow": ["log","info", "error"] }]
+  }
+};
